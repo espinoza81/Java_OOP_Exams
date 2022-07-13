76a2e3ffc51e5ff9e6b0f26547a5191dc7545690
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-13 14:29:28 +0300

    Create MuscleCar.java

---
+++ 03 Java OOP Retake Exam - 20 December 2021/christmasRaces/entities/cars/MuscleCar.java
@@ -0,0 +1,8 @@
+package christmasRaces.entities.cars;
+
+public class MuscleCar extends BaseCar {
+
+    public MuscleCar(String model, int horsePower) {
+        super(model, horsePower, 5000);
+    }
+}
