7193b12fa5a52d78696cad8c82d371c7a67bc8bf
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-07-13 14:29:44 +0300

    Create SportsCar.java

---
+++ 03 Java OOP Retake Exam - 20 December 2021/christmasRaces/entities/cars/SportsCar.java
@@ -0,0 +1,8 @@
+package christmasRaces.entities.cars;
+
+public class SportsCar extends BaseCar {
+
+    public SportsCar(String model, int horsePower) {
+        super(model, horsePower, 3000);
+    }
+}
